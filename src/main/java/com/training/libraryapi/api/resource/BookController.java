92e49f772c2444c813761928f840aa8a35ea4416
Milton <miltonmjcp@gmail.com>
2022-03-21 21:34:43 -0300

    Incluido a forma simples do dto

--- src/main/java/com/training/libraryapi/api/resource/BookController.java
+++ src/main/java/com/training/libraryapi/api/resource/BookController.java
@@ -3,7 +3,6 @@ package com.training.libraryapi.api.resource;
 import com.training.libraryapi.dto.BookDTO;
 import com.training.libraryapi.model.entity.Book;
 import com.training.libraryapi.service.BookService;
-import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,10 +15,17 @@ public class BookController {
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
-    /**Esse método é utilizado para criar um book*/
+
+    /**
+     * Esse método é utilizado para criar um book
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public BookDTO create(@RequestBody BookDTO dto) {
+
+        /**Aqui nós temos um caso que é chegado um Dto, passa para a entitidade
+         * para acessar o banco de dados e retorna os dados da entidade em forma
+         * dto. */
         Book entity = Book.builder()
                 .author(dto.getAuthor())
                 .title(dto.getAuthor())
@@ -34,6 +40,7 @@ public class BookController {
                 .title(entity.getTitle())
                 .isbn(entity.getIsbn())
                 .build();
+
         return bookDTO;
     }
 }
--- src/main/java/com/training/libraryapi/dto/BookDTO.java
+++ src/main/java/com/training/libraryapi/dto/BookDTO.java
@@ -10,7 +10,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BookDTO {
-
+    /*Nós utilizamos um DTO para que ele crie um Json de entrada e saída nas interações
+    * com o cliente para que não seja exposto a camada mais interna da aplicação e seus atributos.*/
     private Long id;
     private String title;
     private String author;
