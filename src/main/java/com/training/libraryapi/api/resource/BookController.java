fbeb6cfdac5a13e54a3acc04735b26aaf90c764c
Milton <miltonmjcp@gmail.com>
2022-03-21 21:53:49 -0300

    Feito conversões entre entidade e dto via modelMapper

--- src/main/java/com/training/libraryapi/api/resource/BookController.java
+++ src/main/java/com/training/libraryapi/api/resource/BookController.java
@@ -3,6 +3,7 @@ package com.training.libraryapi.api.resource;
 import com.training.libraryapi.dto.BookDTO;
 import com.training.libraryapi.model.entity.Book;
 import com.training.libraryapi.service.BookService;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,9 +12,11 @@ import org.springframework.web.bind.annotation.*;
 public class BookController {
 
     private BookService bookService;
+    private ModelMapper modelMapper;
 
-    public BookController(BookService bookService) {
+    public BookController(BookService bookService, ModelMapper modelMapper) {
         this.bookService = bookService;
+        this.modelMapper = modelMapper;
     }
 
     /**
@@ -22,25 +25,16 @@ public class BookController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public BookDTO create(@RequestBody BookDTO dto) {
-
-        /**Aqui nós temos um caso que é chegado um Dto, passa para a entitidade
-         * para acessar o banco de dados e retorna os dados da entidade em forma
-         * dto. */
-        Book entity = Book.builder()
-                .author(dto.getAuthor())
-                .title(dto.getAuthor())
-                .isbn(dto.getIsbn())
-                .build();
-
+        /**Aqui nós temos um caso de conversão entre objetos, onde recebemos um dto
+         * e o convertemos via modelMapper para a o objeto de entidade Book para que
+         * seja feito as transações*/
+        Book entity = modelMapper.map(dto, Book.class);
+        /**Aqui é feito o salvamento dentro de bookService */
         entity = bookService.save(entity);
+        /**Aqui é convetido os valores da entitidade para um um DTO novamente para que seja
+         * entregue um Json com a resposta evitando a exposição do dominio.*/
+        BookDTO responseDto = modelMapper.map(entity, BookDTO.class);
 
-        BookDTO bookDTO = BookDTO.builder()
-                .id(entity.getId())
-                .author(entity.getAuthor())
-                .title(entity.getTitle())
-                .isbn(entity.getIsbn())
-                .build();
-
-        return bookDTO;
+        return responseDto;
     }
 }
